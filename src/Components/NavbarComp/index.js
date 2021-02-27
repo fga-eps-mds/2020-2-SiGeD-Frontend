2c29e3f1c5ffb438148b34cbf09c4458ea43c71e
lucas <lucascandradex14@gmail.com>
2021-02-27 16:25:53 -0300

    add logo to the src folder
    Co-authored-by: Joao Pedro <joaopedroaschaves@gmail.com>

--- src/Components/NavbarComp/index.js
+++ src/Components/NavbarComp/index.js
@@ -1,12 +1,13 @@
 import {Navbar, FormControl, Form, Nav, Button} from 'react-bootstrap';
 import colors from '../../Constants/colors';
 import React from 'react'
+import logo from '../../Assets/logopcgo.png';
 
 const NavbarComp = () => {
     return(
         <>
         <Navbar fixed = 'top' style = {{backgroundColor: colors.primary}}>
-            <Navbar.Brand href="#home"><img style = {{marginLeft: "2vh", height: "8vh"}} src='https://www.policiacivil.go.gov.br/wp-content/uploads/2018/10/logopcgo-dourado.png    '></img></Navbar.Brand>
+            <Navbar.Brand href="#home"><img style = {{marginLeft: "2vh", height: "8vh"}} src={logo}/></Navbar.Brand>
             <Nav className="ml-auto">
             <Nav.Link style = {{fontSize: "3vh", marginRight: "2vh", color: colors.secondary}}>Login</Nav.Link>
             <Nav.Link style = {{fontSize: "3vh", color: colors.secondary}}>Cadastro</Nav.Link>
