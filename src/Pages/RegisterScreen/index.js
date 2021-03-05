b1c69055f81dd11412fe910fa73fa118a47602d2
Thiago <thiago3399@hotmail.com>
2021-03-05 01:41:25 -0300

    Added validation functions to register screen
    Co-authored-by: Davi Marinho <davii_marinho@hotmail.com>
    Signed-off-by: Thiago <thiago3399@hotmail.com>

--- src/Pages/RegisterScreen/index.js
+++ src/Pages/RegisterScreen/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { IoPersonCircleOutline } from 'react-icons/io5';
+import axios from 'axios';
 import TinyButton from '../../Components/TinyButton';
 import styles from './style';
 import RegisterInput from '../../Components/RegisterInput';
-import axios from 'axios';
 
 const RegisterScreen = () => {
   const [cardName, setCardName] = useState('');
@@ -14,26 +14,68 @@ const RegisterScreen = () => {
   const [inputRegister, setInputRegister] = useState('');
   const [inputPassword, setInputPassword] = useState('');
   const [inputConfirmPassword, setInputConfirmPassword] = useState('');
-  const submit = () => {
+  const [valid, setValid] = useState('');
 
+  const validateEmail = (email) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regex.test(email);   
+  }
+
+  const validateName = (name) => {
+    const regex = /^[a-zA-Z ]{2,30}$/;
+      return regex.test(name);
+  } 
+
+  const validatePassword = (pass) => {
+    if(pass.length >= 6){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  const confirmPassword = (pass1, pass2) => {
+    if(pass2 == pass1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  const validateSignUp = (email, name, pass1, pass2) =>{
+    if(validateName(name) && validateEmail(email) && validatePassword(pass1) && confirmPassword(pass1, pass2)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  const submit = () => {
+    if (validateSignUp(inputEmail, inputName, inputPassword, inputConfirmPassword)){
+      console.log('Succes!')
     async function postUser() {
-        try {
-            const response = await axios.post('http://localhost:3001/signUp', {
-                "name": inputName,
-                "email": inputEmail,
-                "enroll": inputRegister,
-                "pass": inputPassword	
-            })
-            .then(function (response) {
-                console.log(response);
-            })
-            } catch (error) {
-                console.error(error);
-            }
+      try {
+        await axios.post('http://localhost:3001/signUp', {
+          name: inputName,
+          email: inputEmail,
+          enroll: inputRegister,
+          pass: inputPassword,
+        })
+          .then((response) => {
+            setValid(response);
+            console.log(response, valid);
+          });
+      } catch (error) {
+        console.error(error);
+      }
     }
 
-    postUser()
-}
+    postUser();
+  }else{
+    console.log('Invalid field inputs')
+  }
+  };
+
   const cancel = () => {
     setInputEmail('');
     setInputRegister('');
