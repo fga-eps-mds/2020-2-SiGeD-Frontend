09532cd08ac2d97e1d5c3a6ab70a0bcc4e37fc02
Thiago <thiago3399@hotmail.com>
2021-03-08 19:43:33 -0300

    Created categories list page on frontend
    Co-authored-by: Lucas <lucascandradex14@gmail.com>
    Signed-off-by: Thiago <thiago3399@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import NavbarComp from './Components/NavbarComp';
 import LoginScreen from './Pages/LoginScreen';
 import RegisterScreen from './Pages/RegisterScreen';
 import ListScreen from './Pages/ListScreen';
 import ClientRegisterScreen from './Pages/ClientRegisterScreen';
+import ListCategories from './Pages/ListCategories';
+import NavbarComp from './Components/NavbarComp';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/cadastro" component={RegisterScreen} />
         <Route path="/usuarios" component={ListScreen} />
         <Route path="/cliente" component={ClientRegisterScreen} />
+        <Route path="/categorias" component={ListCategories} />
       </Switch>
     </Router>
 
