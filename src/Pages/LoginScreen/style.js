d43b5605bccb9f65e9dda20229deb6cc830e6f11
DafneM <moretti.dafne2@gmail.com>
2021-03-09 18:19:10 -0300

    [61] Add mobile responsiveness in LoginScreen.
    Co-authored-by: Davi <davii_marinho@hotmail.com>
    Signed-off-by: DafneM <moretti.dafne2@gmail.com>

--- src/Pages/LoginScreen/style.js
+++ src/Pages/LoginScreen/style.js
@@ -1,4 +1,7 @@
-const styles = {
+import styled from 'styled-components';
+import colors from '../../Constants/colors';
+
+export const styles = {
   background: {
     width: '100vw',
     height: '100vh',
@@ -37,4 +40,41 @@ const styles = {
   },
 };
 
-export default styles;
+export const Background = styled.div` 
+  width: 100vw;
+  height: 100vh;
+  background-color: ${colors.background};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  @media(max-width: 425px){
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+export const Center = styled.div`
+    width: 55vh;
+    height: 55vh;
+    background-color: ${colors.secondary};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+    border-radius: 8px;
+    margin-top: 15vh;
+    margin-bottom: 10vh;
+
+    @media(max-width: 425px){
+    width: 100vw;
+    box-shadow: 0;
+    border-radius: 0;
+  }
+`;
