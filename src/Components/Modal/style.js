2fb5d3f907645b434f7fa7d542c4f78afffd4389
lucas <lucascandradex14@gmail.com>
2021-03-11 20:20:05 -0300

    [65] modal cancel button now working and changed to styled components
    Co-authored-by: Thiago <thiago3399@hotmail.com>

--- src/Components/Modal/style.js
+++ src/Components/Modal/style.js
@@ -1,67 +1,83 @@
-const styles = {
-  modalBox: {
-    display: 'flex',
-    position: 'fixed',
-    width: '600px',
-    height: '600px',
-    top: '50%',
-    left: '50%',
-    marginTop: '-300px', /* Negative half of height. */
-    marginLeft: '-300px', /* Negative half of width. */
-    backgroundColor: 'white',
-    borderRadius: '20px',
-    border: '3px solid black',
-    boxSizing: 'border-box',
-    boxShadow: '5px 5px 5px #6A6262',
-  },
-  modalContent: {
-    margin: '90px 50px',
-    font: 'Open Sans',
-  },
-  colorPreview: {
-    borderRadius: '5%',
-    height: '50px',
-    width: '50px',
-    bottom: '20px',
-    position: 'relative',
-  },
-  campoNome: {
-    display: 'inline-block',
-  },
-  campoCor: {
-    display: 'inline-block',
-    float: 'right',
-  },
+import styled from 'styled-components';
+// x
+export const ModalBox = styled.div`
+  display: flex;
+  position: fixed;
+  width: 600px;
+  height: 600px;
+  top: 50%;
+  left: 50%;
+  margin-top: -300px; /* Negative half of height. */
+  margin-left: -300px; /* Negative half of width. */
+  background-color: white;
+  border-radius: 20px;
+  border: 3px solid black;
+  box-sizing: border-box;
+  box-shadow: 5px 5px 5px #6A6262;
+`;
+// x
+export const ModalContent = styled.div`
+  margin: 90px 50px;
+  font: Open Sans;
+`;
+// x
+export const ModalCampos = styled.div`
+  margin-top: 28px;
+`;
+// x
+export const ModalCampoNome = styled.div`
+  display: inline-block;
+`;
+// x
+export const P = styled.p`
+  margin: 10px;
+  font-size: 1.8vh;
+`;
+
+export const Input = styled.input`
+  border-radius: 10px;
+  border: 3px solid black;
+  text-indent: 10px;
+  font-size: 18px;
+`;
+// x
+export const ModalCampoCor = styled.div`
+  display: inline-block;
+  float: right;
+`;
+// x
+export const CorPreview = styled.div`
+  border-radius: 5%;
+  height: 50px;
+  width: 50px;
+  bottom: 20px;
+  position: relative;
+`;
+// x
+export const ModalCampoDescricao = styled.div`
+  margin-top: 28px;
+`;
+// x
+export const ModalCampoBotao = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+// x
+export const ColorText = styled(P)`
+  position: relative;
+  right: 70px;
+  top: 30px;
+`;
+
+export const styles = {
   input: {
     borderRadius: '10px',
     border: '3px solid black',
     textIndent: '10px',
     fontSize: '18px',
-  },
-  p: {
-    margin: '10px',
-    fontSize: '1.8vh',
-  },
-  campos: {
-    marginTop: '28px',
-  },
-  campoDescricao: {
-    marginTop: '28px',
-  },
-  colorP: {
-    margin: '10px',
-    fontSize: '1.8vh',
-    position: 'relative',
-    right: '70px',
-    top: '30px',
-  },
-  divButtom: {
-    display: 'flex',
-    height: '100%',
-    width: '100%',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
+    resize: 'none',
   },
 };
-
-export default styles;
