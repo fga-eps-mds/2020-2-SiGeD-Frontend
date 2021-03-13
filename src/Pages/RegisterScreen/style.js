bb8c937066656660bfc0f19c02552256fa0faae3
DaviMarinho <davii_marinho@hotmail.com>
2021-03-12 23:32:07 -0300

    Fix the mobile responsiveness of the register screen
    Co-authored-by: Dafne <moretti.dafne2@gmail.com>
    Signed-off-by: DaviMarinho <davii_marinho@hotmail.com>

--- src/Pages/RegisterScreen/style.js
+++ src/Pages/RegisterScreen/style.js
@@ -1,91 +1,141 @@
+import styled from 'styled-components';
+import { IoPersonCircleOutline } from 'react-icons/io5';
 import colors from '../../Constants/colors';
 
-const styles = {
+export const Main = styled.div` 
+    width: 100vw;
+    height: 100vh;
+    background-color: #BFBFBF;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+    @media(max-width: 720px){
+    
+    }
+`;
 
-  container: {
+export const Container = styled.div`
+    width: 65%;
+    height: 60%;
+    background: ${colors.secondary};
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    overflow: hidden;
+    border-radius: 1.5vw;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    @media(max-width: 720px){
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        justify-content: flex-start;
+        padding-top: 10vh;
+    }
+`;
 
-    display: 'flex',
-    background: colors.secondary,
-    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    borderRadius: '1.5vw',
-    justifyContent: 'space-between',
-    width: '65%',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    overflow: 'hidden',
-    marginTop: '15vh',
-    marginBottom: '10vh',
+export const Sidebar = styled.div`
+    width: 30%;
+    height: 100%;
+    background: ${colors.primary};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
+    @media(max-width: 720px){
+        width: 100%;
+        height: 20%;
+        background-color: ${colors.secondary};
+        align-content: center;
+        justify-content: center;
+   
+    }
+`;
 
-  },
+export const RightBox = styled.div`
+    width: 70%;
+    height: 90%;
+    background: ${colors.secondary};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    @media(max-width: 720px){
+        width: 100%;
+        height: 80%;
+    }
+`;
 
-  sidebar: {
+export const ColumnText = styled.div`
+    width: 90%;
+    height: 90%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    margin-top: 10px;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    @media(max-width: 720px){
+        align-items: center;
+        width: 90%;
+        height: 80%;
+        justify-content: space-between;
+        align-content: center;
+    }
+`;
 
-    background: colors.primary,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '30%',
-    overflow: 'hidden',
-  },
+export const DivButtom = styled.div`
+    width: 100%;
+    height: 10%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    margin-right: 10%;
+    margin-top: 5%;
+    flex-direction: row;
+    @media(max-width: 720px){
+        height: 20%;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        align-content: space-around;
+        margin-right: 0%;
+        
+    }
+`;
 
-  main: {
+export const SidebarText = styled.div`
+    color: ${colors.secondary};
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    @media(max-width: 720px){
+        visibility: hidden;
+        height: 0%;
+    }
+    p {
+        font: Open Arial;
+        font-size: 1.5vw;
+    }
+`;
 
-    backgroundColor: '#BFBFBF',
-    alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    height: '100vh',
-    width: '100vw',
-    aligncontent: 'center',
-  },
+export const Icon = styled(IoPersonCircleOutline)`
+    width: 100%;
+    height: 40%;
+    color: ${colors.secondary};
+    margin-top: 10px;
+    @media(max-width: 720px){
+        width: 100%;
+        height: 100%;
+        color: ${colors.primary};
+    }
+`;
 
-  row: {
-
-    clear: 'both',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    width: '70%',
-    height: '100%',
-    overflow: 'hidden',
-
-  },
-
-  peopleIcon: {
-    height: '10vw',
-    width: '10vw',
-    marginTop: '20%',
-    color: colors.secondary,
-    marginBottom: '8%',
-  },
-
-  sidebarDiv: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-
-  sidebarText: {
-    color: colors.secondary,
-    font: 'Open Sans',
-    fontSize: '1.5vw',
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-
-  divButtom: {
-    display: 'flex',
-    height: '100%',
-    width: '100%',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-  },
-
-  divInputs: {
-    width: '100%',
-  },
-
-};
-
-export default styles;
+export const InputLine = styled.div`
+    width: 100%;
+    height: 10%;
+    @media(max-width: 720px){
+    
+    }
+`;
