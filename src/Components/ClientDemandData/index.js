ce91ce8d8b6d6f33b085fef29552f763f2f48fb7
Thiago <thiago3399@hotmail.com>
2021-04-14 03:45:17 -0300

    [186] Merge whith development, fix client prifile and client demand data and it's design
    Co-authored-by: Joao Pedro <joaopedroaschaves@gmail.com>
    Signed-off-by: Thiago <thiago3399@hotmail.com>

--- src/Components/ClientDemandData/index.js
+++ src/Components/ClientDemandData/index.js
@@ -1,14 +1,12 @@
-import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
 import {
   DemandCard, DemandTitle, SectorName, ProcessNumber,
   DemandCreatedAt, CategoryField, CategoryName,
-} from '../DemandData/Style';
-import colors from '../../Constants/colors';
+} from './Style';
 
 const ClientDemandData = ({ demand, sectors }) => {
   console.log('oi', demand, sectors);
-  const sectorName = sectors?.filter((sectorByID) => (sectorByID._id
+  const sectorName = sectors?.map((sectorByID) => (sectorByID._id
     === demand.sectorHistory[demand.sectorHistory.length - 1].sectorID));
 
   const renderDemandCategories = () => (demand.categoryID?.map((category) => (
@@ -16,39 +14,27 @@ const ClientDemandData = ({ demand, sectors }) => {
   )));
 
   return (
-    <>
-      { sectors && demand
-        && (
-          <DemandCard
-            as={Link}
-            to={`/visualizar/${demand._id}`}
-            style={{
-              textDecorationLine: 'none',
-              color: colors.text,
-            }}
-          >
-            <DemandTitle>
-              {demand.name}
-            </DemandTitle>
-            <SectorName>
-              Setor:
-              {sectorName[0].name}
-            </SectorName>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <ProcessNumber>
-                Nº do Processo:
-                {demand.process}
-              </ProcessNumber>
-              <DemandCreatedAt>
-                {format(new Date(demand.createdAt), 'dd/MM/yyyy')}
-              </DemandCreatedAt>
-            </div>
-            <CategoryField>
-              {renderDemandCategories()}
-            </CategoryField>
-          </DemandCard>
-        )}
-    </> 
+    <DemandCard>
+      <DemandTitle>
+        {demand.name}
+      </DemandTitle>
+      <SectorName>
+        Setor:
+        {sectorName[0].name}
+      </SectorName>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <ProcessNumber>
+          Nº do Processo:
+          {demand.process}
+        </ProcessNumber>
+        <DemandCreatedAt>
+          {format(new Date(demand.createdAt), 'dd/MM/yyyy')}
+        </DemandCreatedAt>
+      </div>
+      <CategoryField>
+        {renderDemandCategories()}
+      </CategoryField>
+    </DemandCard>
   );
 };
 
