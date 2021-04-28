9f4068474b3fd4b8c7f31cee6c72256669343814
yukioz <yukio.link@gmail.com>
2021-04-28 19:25:59 -0300

    [215] Reduce duplication and improve code.
    Co-authored-by: Davi Marinho <davii_marinho@hotmail.com>
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- src/Components/DemandHistory/index.js
+++ src/Components/DemandHistory/index.js
@@ -2,27 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { format } from 'date-fns';
 import {
-  TimelineOppositeContent,
   TimelineItem,
-  TimelineSeparator,
   TimelineConnector,
-  TimelineContent,
+  TimelineSeparator,
   TimelineDot,
+  TimelineOppositeContent,
+  TimelineContent,
 } from '@material-ui/lab';
 import colors from '../../Constants/colors';
 import {
   TimeDiv, UpdateDiv, UserName, Tag,
 } from './Style';
 import { getUser } from '../../Services/Axios/userServices';
+import { getCategories } from '../../Services/Axios/demandsServices';
 import { useProfileUser } from '../../Context';
 import ModalHistory from '../ModalHistory';
-import { getCategories } from '../../Services/Axios/demandsServices';
 
 const DemandHistory = ({ show, handleClose, demand }) => {
-  const { startModal } = useProfileUser();
   const [users, setUsers] = useState([]);
-  const [categories, setCategories] = useState([]);
+  const { startModal } = useProfileUser();
   const history = useHistory();
+  const [categories, setCategories] = useState([]);
 
   const getUserFromApi = async () => {
     await getUser('users', startModal)
@@ -39,20 +39,20 @@ const DemandHistory = ({ show, handleClose, demand }) => {
     getCategoriesFromApi();
   }, []);
 
-  const dictionary = (word) => {
-    if (word === 'name') {
+  const dictionaryDemand = (wordChanged) => {
+    if (wordChanged === 'name') {
       return 'nome';
     }
-    if (word === 'description') {
+    if (wordChanged === 'description') {
       return 'descrição';
     }
-    if (word === 'category') {
+    if (wordChanged === 'category') {
       return 'categoria';
     }
-    if (word === 'process') {
+    if (wordChanged === 'process') {
       return 'processo';
     }
-    return word;
+    return wordChanged;
   };
 
   const updateData = (after, label) => {
@@ -70,8 +70,8 @@ const DemandHistory = ({ show, handleClose, demand }) => {
     return after;
   };
 
-  const showHistory = () => demand?.demandHistory.map((update) => {
-    const findedUser = users?.filter((user) => user?._id === update.userID);
+  const showHistory = () => demand?.demandHistory.map((updateDemand) => {
+    const findedUser = users?.filter((user) => user?._id === updateDemand.userID);
 
     return (
       <TimelineItem style={{ marginLeft: '8%' }}>
@@ -82,9 +82,9 @@ const DemandHistory = ({ show, handleClose, demand }) => {
         </TimelineSeparator>
         <TimelineContent style={{ width: '100%' }}>
           <TimeDiv>
-            {format(new Date(update.date), 'dd/MM/yyyy')}
+            {format(new Date(updateDemand.date), 'dd/MM/yyyy')}
             {' - Atualização de '}
-            {dictionary(update.label)}
+            {dictionaryDemand(updateDemand.label)}
             {' por '}
             <UserName onClick={() => history.push('/usuarios')}>
               {findedUser[0]?.name}
@@ -93,11 +93,11 @@ const DemandHistory = ({ show, handleClose, demand }) => {
           <UpdateDiv>
             <p style={{ color: 'red' }}>
               {'Antes: '}
-              {updateData(update.before, update.label)}
+              {updateData(updateDemand.before, updateDemand.label)}
             </p>
             <p style={{ color: 'blue' }}>
               {'Depois: '}
-              {updateData(update.after, update.label)}
+              {updateData(updateDemand.after, updateDemand.label)}
             </p>
           </UpdateDiv>
         </TimelineContent>
